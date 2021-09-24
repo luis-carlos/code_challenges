f07bce7aca81a3bd606a0847e65aa2ff7949fb39
Luis C Garcia <rooster.wally@gmail.com>
2021-09-24 00:09:44 -0500

    Add files via upload

---
+++ find_message.js
@@ -0,0 +1,11 @@
+var s = "cdeo";
+var a = [3, 2, 0, 1];
+var k = 0;
+var return_s = s[0];
+
+do {
+    return_s = return_s.concat(s[a[k]]);
+    k = a[k];
+} while (a[k] !== 0)
+
+console.log(return_s);
