ed6587c40145266a5bd930b5964b200e4c4d5e82
Luis Carlos Garcia <luis.c.garcia@oracle.com>
2021-09-30 00:46:17 -0500

    Adding 'Find visible nodes'

---
+++ find_visible_nodes.js
@@ -0,0 +1,41 @@
+class Node {
+    constructor(val) {
+        this.key = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+var helperFunction = function(compare,node){
+    let offSet = 0;
+    if (node === null)
+        return 0;
+    if (node.key >= compare)
+        offSet = 1;
+    let newHigh = (node.key >= compare) ? node.key : compare;
+    return offSet + helperFunction(newHigh, node.left) + helperFunction(newHigh, node.right);
+}
+
+var goodNodes = function(node) {
+    return 1 + helperFunction(node.key, node.left) + helperFunction(node.key, node.right);
+}
+
+root = new Node(25);
+root.left = new Node(15);
+root.left.left = new Node(10);
+root.left.left.left = new Node(4);
+root.left.left.right = new Node(12);
+root.left.right = new Node(22);
+root.left.right.left = new Node(18);
+root.left.right.right = new Node(24);
+root.right = new Node(50);
+root.right.left = new Node(35);
+root.right.left.left = new Node(31);
+root.right.left.right = new Node(44);
+root.right.right = new Node(70);
+root.right.right.left = new Node(66);
+root.right.right.right = new Node(90);
+
+console.log(goodNodes(root));
+
