5a8401cc655d92f2e7cdf06a5bb821192951fa98
Luis Carlos Garcia <luis.c.garcia@oracle.com>
2021-10-17 02:01:17 -0500

    Bug fixes

--- find_smalles_positive_integer_missing_in_array.js
+++ find_smalles_positive_integer_missing_in_array.js
@@ -1,18 +1,17 @@
-var solution = function(intArray){
-    intArray.sort((a,b) => a-b);
-    console.log(intArray);
+var solution = function(A){
+    A.sort((a,b) => a-b);
+    console.log(A);
 
-    for (let i = 0; i < intArray.length; i++){
-        if (i === (intArray.length - 1)) {
-            if (intArray[i] <= 0) 
+    for (let i = 0; i < A.length; i++){
+        if (i === (A.length - 1)) {
+            if (A[i] <= 0) 
             return 1;
         }
-        if ((intArray[i] >= 0) && (intArray[i] !== intArray[i + 1]) && (intArray[i + 1] !== (intArray[i] + 1))) {
-            console.log(`[${i}]${intArray[i]} different than ${intArray[i+1]}`);
-            return (intArray[i] + 1);
-            // return (intArray[i] + 1)
+        if ((A[i] >= 0) && (A[i] !== A[i + 1]) && (A[i + 1] !== (A[i] + 1))) {
+            console.log(`[${i}]${A[i]} different than ${A[i+1]}`);
+            return (A[i] + 1);
         }
     }
 }
 
-console.log(solution([-1, -3]));
+console.log(solution([1]));
