7990544c442cb4c9e655c1826a3f82b6fe4624fe
Luis Garcia <luis.c.garcia@oracle.com>
2022-01-22 00:17:04 -0600

    Sample code challenges from MS and AgileEngine

---
+++ agileengine_rest_api_candidates.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// Your code starts here. Placeholders for .get and .post are provided for
+//  your convenience.
+
+app.post('/candidates', function(req, res) {
+  // ...
+});
+
+app.get('/candidates/search', function(req, res) {
+  // ...
+});
+
+app.listen(process.env.HTTP_PORT || 3000);
