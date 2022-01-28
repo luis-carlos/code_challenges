3150c7c5d6dde18fd742230974acd0a901d3b739
Luis Garcia <luis.c.garcia@oracle.com>
2022-01-28 00:35:35 -0600

    Adding c3 examples

---
+++ c3_closest_cities.js
@@ -0,0 +1,17 @@
+/* 
+Se entregan varios arreglos con los nombres de ciudades y coordinadas X, Y
+c = ['fastcity', 'bigbanana', 'xyz']
+x = [23, 23, 23]
+y = [1, 20, 20]
+
+Devolver un arreglo que regrese la ciudad más cercana de cada una
+
+Para considerarse cercana tienen que estar en la misma coordenada X o Y. Si más de una
+ciudad cumple ese criterio, el desempate será para determinar la que esté más cerca 
+alfabéticamente
+
+
+
+
+
+*/
---
+++ c3_string_permutations.js
@@ -0,0 +1,20 @@
+/* 
+
+Si se entrega un valor n, encontrar todas la palabras con dicha longitud que cumplan las siguientes reglas:
+
+- Cada letra es una vocal
+- La letra a sólo puede estar seguida de e
+- La letra e sólo puede estar seguida de a o i
+- La letra i no puede estar cerca de otra i
+- La letra o sólo puede estar seguida de una i o una u
+- La letra u sólo puede estar seguida de una a
+
+Ejemplos
+
+n = 2
+respuesta = 10 {io, iu, oi, ou, ua, ae, ea, ei, ia, ie}
+
+n = 3
+respuesta = 19 {iua, oia, oie, oio, oiu, oua, uae, aea, aei, eae, eia, eie, eio, eiu, iae, iea, iei, ioi, iou }
+
+*/
