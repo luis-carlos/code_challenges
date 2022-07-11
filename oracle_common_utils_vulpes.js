79b3f35ae5d5a1b2b6424053d0d9afb5d830ed5a
Luis Garcia <luis.c.garcia@oracle.com>
2022-07-11 00:43:46 -0500

    Studying material

--- oracle_common_utils_vulpes.js
+++ oracle_common_utils_vulpes.js
@@ -1,88 +1,17 @@
 const lo = require('lodash');
 
-const SOURCES = {
-    NONE: {
-      label: 'None',
-      isAddressSrc: true,
-    },
-    BUILTWITH: {
-      label: 'BuiltWith',
-      isAddressSrc: false,
-    },
-    CALCULATED: {
-      label: 'Calculated',
-      isAddressSrc: false,
-    },
-    CRUNCHBASE: {
-      label: 'Crunchbase',
-      isAddressSrc: true,
-    },
-    DATAFOX_OVERRIDE: {
-      label: 'DataFox Override',
-      isAddressSrc: false,
-    },
-    EDGAR: {
-      label: 'Edgar',
-      isAddressSrc: true,
-    },
-    FLEXIBLE_DATA: {
-      label: 'Flexible Data',
-      isAddressSrc: true,
-    },
-    FORM_5500: {
-      label: 'Form 5500',
-      isAddressSrc: false,
-    },
-    HG_DATA: {
-      label: 'HG Data',
-      isAddressSrc: false,
-    },
-    NAICS_DOT_COM: {
-      label: 'naicsDotCom',
-      isAddressSrc: false,
-    },
-    INDEED: {
-      label: 'Indeed',
-      isAddressSrc: false,
-    },
-    INTRINIO: {
-      label: 'Intrinio',
-      isAddressSrc: false,
-    },
-    TICKER: {
-      label: 'Ticker',
-      isAddressSrc: false,
-    },
-    UK_COMPANIES_HOUSE: {
-      label: 'UkCompaniesHouse',
-      isAddressSrc: true,
-    },
-    URL: {
-      label: 'URL',
-      isAddressSrc: true,
-    },
-    WEBSITE: {
-      label: 'Website',
-      isAddressSrc: true,
-    },
-    ZOOMINFO: {
-      label: 'ZoomInfo',
-      isAddressSrc: true,
-    },
-    SIMILARWEB: {
-      label: 'SimilarWeb',
-      isAddressSrc: false,
-    },
-    SIGNAL_ACQUISITION: {
-      label: 'SignalAcquisition',
-      isAddressSrc: false,
-    },
-    OPENCORPORATES: {
-      label: 'OpenCorporates',
-      isAddressSrc: true,
-    },
-};
-const result = lo.mapValues(SOURCES, 'label');
+const children = [
+  {
+    "company": "5130efc78989846a36000ad8"
+  },
+  {
+    "company": "55887190cf1e140e4b000155"
+  },
+  {
+    "company": "5dba656a10ebcfda05e3f878"
+  }
+];
+const result = lo.map(children, 'company');
 
 
 
